29581647773807f418733cb4f87ad80c6d5348eb
SilverSun000 <williamcahill78@gmail.com>
2025-09-25 17:50:46 -0500

    added jdtls to config

--- init.lua
+++ init.lua
@@ -462,6 +462,11 @@ require('lazy').setup({
     end,
   },
 
+  {
+    'mfussenegger/nvim-jdtls',
+    ft = { 'java' },
+  },
+
   -- LSP Plugins
   {
     -- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
@@ -623,6 +628,35 @@ require('lazy').setup({
               vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf })
             end, '[T]oggle Inlay [H]ints')
           end
+        end,
+      })
+
+      vim.api.nvim_create_autocmd('FileType', {
+        pattern = { 'java' },
+        callback = function()
+          local jdtls = require 'jdtls'
+
+          local home = os.getenv 'HOME'
+          local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
+          local workspace_dir = home .. '/.local/share/eclipse/' .. project_name
+
+          local config = {
+            cmd = { 'jdtls', '-data', workspace_dir },
+            root_dir = require('jdtls.setup').find_root {
+              '.git',
+              'mvnw',
+              'gradlew',
+              'pom.xml',
+              'build.gradle',
+            },
+            settings = {
+              java = {
+                signatureHelp = { enabled = true },
+                contentProvider = { preferred = 'fernflower' },
+              },
+            },
+          }
+          jdtls.start_or_attach(config)
         end,
       })
 
