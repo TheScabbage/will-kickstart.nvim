486e00ea0655ebd1efd0b51d13fcd5bbde00d234
SilverSun000 <williamcahill78@gmail.com>
2025-09-26 14:14:25 -0500

    added java support

--- init.lua
+++ init.lua
@@ -462,11 +462,6 @@ require('lazy').setup({
     end,
   },
 
-  {
-    'mfussenegger/nvim-jdtls',
-    ft = { 'java' },
-  },
-
   -- LSP Plugins
   {
     -- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
@@ -480,6 +475,9 @@ require('lazy').setup({
       },
     },
   },
+
+  { 'mfussenegger/nvim-jdtls' },
+
   {
     -- Main LSP Configuration
     'neovim/nvim-lspconfig',
@@ -978,7 +976,7 @@ require('lazy').setup({
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
+      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'java' },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
